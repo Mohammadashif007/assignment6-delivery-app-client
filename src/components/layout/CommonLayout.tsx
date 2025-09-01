cf5c1b0b0cf9b84c47b2f887a7c577f900ca3bf3
Mohammad Ashif <mohammadashif510@gmail.com>
2025-09-01 10:02:20 +0600

    navbar and footer added

---
+++ src/components/layout/CommonLayout.tsx
@@ -0,0 +1,19 @@
+import type { ReactNode } from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+type TProps = {
+    children: ReactNode;
+};
+
+const CommonLayout = ({ children }: TProps) => {
+    return (
+        <div className="min-h-screen flex flex-col">
+            <Navbar></Navbar>
+            <div className="grow-1">{children}</div>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default CommonLayout;
